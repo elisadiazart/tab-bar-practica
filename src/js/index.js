838ecad3fbe0111db58373406e0e8680c12fe39b
Elisa Diaz <71611440+elisadiazart@users.noreply.github.com>
2023-02-23 16:10:13 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// El styles lo importamos aquí, ya se carga después al compilar todo
+import '../scss/styles.scss';
+import { sayHello } from './demo.js';
+
+import catImage from '../assets/images/cat.jpg';
+
+// const img = document.createElement('img')
+// img.src = catImage
+
+sayHello();
