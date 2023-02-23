06367dc546e91b0a2c13bba4f21d75f6f6263dea
Elisa <elisadiazartcontact@gmail.com>
2023-02-23 18:46:17 +0100

    23.02.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-import catImage from '../assets/images/cat.jpg';
-
+// import catImage from '../assets/images/cat.jpg';
 // const img = document.createElement('img')
 // img.src = catImage
 
-sayHello();
+const iconsBar = document.getElementById('icons');
+const rootStyes = document.documentElement.style;
+
+iconsBar.addEventListener('click', e => {
+  if (e.target.id === 'icons') return;
+  [...iconsBar.children].forEach(icon => {
+    icon.classList.remove('nav__li--active');
+  });
+  e.target.classList.add('nav__li--active');
+  rootStyes.setProperty('--icon-color', `${e.target.dataset.color}`);
+  rootStyes.setProperty('--selected-icon', `${e.target.offsetLeft}px`);
+  rootStyes.setProperty(
+    '--text-position',
+    `-${[...iconsBar.children].indexOf(e.target)}00%`
+  );
+});
